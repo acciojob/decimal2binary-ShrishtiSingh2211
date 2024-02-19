2092f38d71634832e51be3c709891460beebe583
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-02-19 13:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
-	let decimal = prompt('please insert decimal number');
-	let i = 0;
-	 let binaryNum = new Array();
-    while (num > 0) { 
-  
-        // storing remainder in binary array 
-        binaryNum[i] = num % 2; 
-        num = Math.floor(num / 2); 
-        i++; 
-    } 
-	for (let j = i - 1; j >= 0; j--) 
-        console.log(binaryNum[j]); 
+	 let i = 0;
+  let binaryNum = new Array();
+  while (num > 0) { 
+    binaryNum[i] = num % 2; 
+    num = Math.floor(num / 2); 
+    i++; 
+  } 
+  let binaryString = '';
+  for (let j = i - 1; j >= 0; j--) 
+    binaryString += binaryNum[j];
+  return binaryString;
 }
 
 window.decimalToBinary = decimalToBinary;
